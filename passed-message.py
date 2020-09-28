cb137e579e50bd8b15d96b38ed3c6afa78e429ee
mikoloism <mikoloism.github@gmail.com>
2020-09-28 20:24:24 +0330

    created check os type

--- passed-message.py
+++ passed-message.py
@@ -1,3 +1,10 @@
-from os import system as bash;
-bash('clear');
-print('file pass! :)');
+from os import system as bash
+
+# check operand system name and architecture
+os = 'linux'
+if os === 'win':
+	bash('cls')
+else:
+	bash('clear')
+
+print('file pass! :)')
