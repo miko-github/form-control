18c1ad42a54ccd516a62cea44be043153a554cb0
mikoloism <mikoloism.github@gmail.com>
2020-10-11 15:49:40 +0330

    create classment scripts

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+class FormCTRL {
+	constructor(
+		selector = document,
+		{
+			type = 'text',
+			pattern = undefined,
+			leng = undefined,
+			onValid = undefined,
+			onInValid = undefined,
+		} = options,
+	) {
+		this.selector = selector;
+		this.options = { type, pattern, leng, onValid, onInValid };
+	}
+}
+
 dock.ready(() => {
 	let ctrl = dock._class('form-control', 0);
 	let input = ctrl.firstElementChild.children[1];
@@ -8,23 +24,15 @@ dock.ready(() => {
 			if (regex.txt(e.key) || e.key === ' ') {
 				if (regex.email(v)) {
 					e.target.parentElement.classList.add('valid');
-					e.target.parentElement.classList.remove(
-						'in-valid',
-					);
+					e.target.parentElement.classList.remove('in-valid');
 				} else {
-					e.target.parentElement.classList.add(
-						'in-valid',
-					);
-					e.target.parentElement.classList.remove(
-						'valid',
-					);
+					e.target.parentElement.classList.add('in-valid');
+					e.target.parentElement.classList.remove('valid');
 				}
 			}
 		};
 	});
-	input.onfocus = (e) =>
-		e.target.parentElement.classList.add('focus');
+	input.onfocus = (e) => e.target.parentElement.classList.add('focus');
 	input.onblur = (e) =>
-		e.target.value < 1 &&
-		e.target.parentElement.classList.remove('focus');
+		e.target.value < 1 && e.target.parentElement.classList.remove('focus');
 });
